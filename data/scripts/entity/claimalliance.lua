6d77a7a5a630b4470c36c7319449f4ce4d730b17
Artem Romanko <rinart73@gmail.com>
2019-05-09 06:42:34 +0200

    1.1.2 * Now when claiming a gate, faction will be checked how many gates they can own. Claimed gates now count towards overall faction gate count.

--- data/scripts/entity/claimalliance.lua
+++ data/scripts/entity/claimalliance.lua
@@ -1,12 +1,33 @@
+local Azimuth = include("azimuthlib-basic")
+local GateFounderConfig
+
+local gatefounder_initialize = ClaimFromAlliance.initialize
+function ClaimFromAlliance.initialize()
+    -- load config
+    local configOptions = {
+      MaxGatesPerFaction = { default = 5, min = 0, format = "floor", comment = "How many gates can each faction found." }
+    }
+    GateFounderConfig = Azimuth.loadConfig("GateFounder", configOptions)
+
+    if gatefounder_initialize then gatefounder_initialize() end -- in case other mod will define it
+end
+
+local gatefounder_claim = ClaimFromAlliance.claim
 function ClaimFromAlliance.claim()
     if not ClaimFromAlliance.interactionPossible(callingPlayer) then return end
     local faction, ship, player = getInteractingFaction(callingPlayer)
     if not faction then return end
 
     local entity = Entity()
-    entity.factionIndex = faction.index
-    
+
     if entity.hasScript and entity:hasScript("data/scripts/entity/gate.lua") then -- claim both gates at once
+        local gateCount = faction:getValue("gates_founded") or 0
+        if gateCount >= GateFounderConfig.MaxGatesPerFaction then
+            player:sendChatMessage("", 1, "Reached the maximum amount of founded gates!"%_t)
+            return
+        end
+        faction:setValue("gates_founded", gateCount + 1)
+    
         local x, y = Sector():getCoordinates()
         local wormhole = entity:getWormholeComponent()
         local tx, ty = wormhole:getTargetCoordinates()
@@ -23,5 +44,5 @@ function ClaimFromAlliance.claim()
         end
     end
 
-    terminate()
+    gatefounder_claim() -- resume vanilla
 end
